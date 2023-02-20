73885778d2dfa385306786e50e5bb2144f8c7e67
anjiboss <nhu.nhu.face@gmail.com>
2023-02-20 14:31:24 +0900

    add loading

--- src/components/MainApp.tsx
+++ src/components/MainApp.tsx
@@ -1,21 +1,50 @@
-import React from "react";
+import React, { useEffect } from "react";
 import MarkDownInput from "./MarkdownInput";
+import { invoke } from "@tauri-apps/api";
+import ReactLoading from "react-loading";
 
 const MainApp: React.FC = () => {
     const [text, setText] = React.useState<string>("");
+    const [loading, setLoading] = React.useState<boolean>(true);
+
+    useEffect(() => {
+        const getTempText = async () => {
+            const text = await invoke("get_temp_content");
+            setTimeout(() => {
+                setText(text as string);
+                setLoading(false);
+            }, 500);
+        };
+
+        getTempText();
+    }, []);
+
     return (
         <>
-            <MarkDownInput
-                value={text}
-                onChange={(value) => setText(value)}
-                style={{
-                    height: "100vh",
-                }}
-                inputStyle={{
-                    height: "100vh",
-                }}
-                // autofocus
-            />
+            {loading ? (
+                <div
+                    style={{
+                        display: "flex",
+                        justifyContent: "center",
+                        alignItems: "center",
+                        height: "80vh",
+                    }}
+                >
+                    <ReactLoading type="spinningBubbles" color="#2a9134" />
+                </div>
+            ) : (
+                <MarkDownInput
+                    value={text}
+                    onChange={(value) => setText(value)}
+                    style={{
+                        height: "100vh",
+                    }}
+                    inputStyle={{
+                        height: "100vh",
+                    }}
+                    // autofocus
+                />
+            )}
         </>
     );
 };
